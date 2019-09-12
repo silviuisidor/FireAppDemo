31cbc04ecc7590191c18102cbd7dccbd828b136d
Silviu <silviu@isidors.com>
2019-09-12 10:37:46 +0300

    Initial Commit

---
+++ FireAppUpdate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireAppUpdate
+//
+//  Created by Silviu on 12/09/2019.
+//  Copyright © 2019 Silviu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
